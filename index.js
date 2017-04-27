93f4fadc73f0b11c0a3c88c5bfbdd1f5c607c7cc
David Eliason <davethemaker@gamil.com>
2017-04-27 13:57:10 -0700

    successfully create socket handshake between client and server transferring the slider value to process.stdout output

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
 const express 	= require('express');
 const app 		= express();
 
-const http 		= require('http');
-const server 	= http.createServer(app);
+const http 		= require('http').Server(app);
+const io 		= require('socket.io')(http);
 
-app.use(express.static(__dirname + '/public'));
+
+
+// app.use(express.static(__dirname + '/public'));
 app.get('/',function(req,res){
 	// res.writeHead(200,{'content-type': 'text/html'});
 	res.sendFile(__dirname + '/index.html');
+});
 
+io.on('connection',function(socket){
+   socket.on('ledValue',function(data){
+   	console.log("new data" + data);
+   });
 });
 
 
-server.listen(8000,function(){
+http.listen(8000,function(){
 	console.log("listening at 8000");
 });
